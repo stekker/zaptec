2e665001f41fd47c87914e18fb11b8fb87583bbc
Martijn Versluis <martijnversluis@users.noreply.github.com>
2022-12-05 20:05:34 +0100

    Add Zaptec::Client#state

--- StekkerWeb/gems/zaptec/lib/zaptec/charger.rb
+++ StekkerWeb/gems/zaptec/lib/zaptec/charger.rb
@@ -3,6 +3,7 @@ module Zaptec
     attr_reader :id,
                 :name,
                 :device_id,
+                :device_type,
                 :installation_name,
                 :installation_id
 
@@ -10,6 +11,7 @@ module Zaptec
       id:,
       name:,
       device_id:,
+      device_type:,
       installation_name:,
       installation_id:
     )
@@ -17,6 +19,7 @@ module Zaptec
       @id = id
       @name = name
       @device_id = device_id
+      @device_type = device_type
       @installation_name = installation_name
       @installation_id = installation_id
     end
@@ -26,6 +29,7 @@ module Zaptec
         id: data.fetch("Id"),
         name: data.fetch("Name"),
         device_id: data.fetch("DeviceId"),
+        device_type: data.fetch("DeviceType"),
         installation_name: data.fetch("InstallationName"),
         installation_id: data.fetch("InstallationId")
       )
